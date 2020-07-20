842d30f5df7756525ce22822a7e91baf554fe5fd
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-07-21 03:33:11 +0530

    chore: docs for command structures

--- src/lib/structures/CommandStore.ts
+++ src/lib/structures/CommandStore.ts
@@ -1,7 +1,16 @@
 import { Command } from './Command';
 import { AliasStore, PieceConstructor, Client } from '@klasa/core';
 
+/**
+ * Stores all Command pieces
+ * @since 0.0.1
+ */
 export class CommandStore extends AliasStore<Command> {
+	/**
+	 * Constructs the Command Store for use
+	 * @since 0.0.1
+	 * @param client The framework client
+	 */
 	public constructor(client: Client) {
 		super(client, 'commands', Command as PieceConstructor<Command>);
 	}
