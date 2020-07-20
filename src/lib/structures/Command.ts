d90e9f8e05d0586ec04e5b99b24cb1389da8f520
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-07-21 03:33:11 +0530

    chore: docs for command structures

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -8,25 +8,117 @@ import { RateLimitManager } from '@klasa/ratelimits';
 export type CooldownLevel = 'author' | 'channel' | 'guild';
 
 export abstract class Command extends AliasPiece {
+	/**
+	 * Permissions required by the bot to run the command
+	 * @since 0.0.1
+	 */
 	public requiredPermissions: Permissions;
+
+	/**
+	 * Delete command's response if the trigger message was deleted
+	 * @since 0.0.1
+	 */
 	public deletable: boolean;
+
+	/**
+	 * A basic summary about the command
+	 * @since 0.0.1
+	 */
 	public description: string;
+
+	/**
+	 * Longer version of command's summary and how to use it
+	 * @since 0.0.1
+	 */
 	public extendedHelp: string;
+
+	/**
+	 * Full category name of the command
+	 * @since 0.0.1
+	 */
 	public fullCategory: string[];
+
+	/**
+	 * Allow disabling of the command in a guild or not
+	 * @since 0.0.1
+	 */
 	public guarded: boolean;
+
+	/**
+	 * Whehter to show the command in the help message or not
+	 * @since 0.0.1
+	 */
 	public hidden: boolean;
+
+	/**
+	 * If the command will only work in NSFW channels
+	 * @since 0.0.1
+	 */
 	public nsfw: boolean;
+
+	/**
+	 * Required level of permission to use the command
+	 * @since 0.0.1
+	 */
 	public permissionLevel: number;
+
+	/**
+	 * Number of re-prompts of an argument
+	 * @since 0.0.1
+	 */
 	public promptLimit: number;
+
+	/**
+	 * Time allowed for re-prompts
+	 * @since 0.0.1
+	 */
 	public promptTime: number;
+
+	/**
+	 * Whether to have flag support for the command
+	 * @since 0.0.1
+	 */
 	public flagSupport: boolean;
+
+	/**
+	 * Allow use of quoted strings for arguments
+	 * @since 0.0.1
+	 */
 	public quotedStringSupport: boolean;
+
+	/**
+	 * Which type of channel the command can execute in
+	 * @since 0.0.1
+	 */
 	public runIn: ChannelType[];
+
+	/**
+	 * Whether the command has sub-commands or not
+	 * @since 0.0.1
+	 */
 	public subcommands: boolean;
+
 	// todo - public usage: CommandUsage;
+
+	/**
+	 * Whether the cooldown applies to author, channel or guild
+	 * @since 0.0.1
+	 */
 	public cooldownLevel: CooldownLevel;
+
+	/**
+	 * The time and limit for cooldown
+	 * @since 0.0.1
+	 */
 	public cooldowns: RateLimitManager;
 
+	/**
+	 * @since 0.0.1
+	 * @param store The command store
+	 * @param directory The base directory to the pieces folder
+	 * @param file The path from the pieces folder to the command file
+	 * @param options Optional Command settings
+	 */
 	public constructor(store: CommandStore, directory: string, files: readonly string[], options: CommandOptions = {}) {
 		super(store, directory, files, options);
 		options = options as Required<CommandOptions>;
@@ -55,14 +147,28 @@ export abstract class Command extends AliasPiece {
 		this.cooldowns = new RateLimitManager(options.cooldown as number, options.bucket as number);
 	}
 
+	/**
+	 * The main category for the command
+	 * @since 0.0.1
+	 * @readonly
+	 */
 	public get category(): string {
 		return this.fullCategory[0] || 'General';
 	}
 
+	/**
+	 * The sub category for the command
+	 * @since 0.0.1
+	 * @readonly
+	 */
 	public get subCategory(): string {
 		return this.fullCategory[1] || 'General';
 	}
 
+	/**
+	 * Defines the JSON.stringify behavior of the command
+	 * @returns {Object}
+	 */
 	public toJSON(): Record<string, any> {
 		return {
 			...super.toJSON(),
@@ -92,6 +198,13 @@ export abstract class Command extends AliasPiece {
 }
 
 export interface Command {
+	/**
+	 * The run method to be overwritten in commands
+	 * @since 0.0.1
+	 * @param message The command message mapped on top of the message used to trigger this command
+	 * @param params The fully resolved parameters based on your usage / usageDelim
+	 * @returns Should return the response message whenever possible
+	 */
 	run?(message: Message, params: any[]): Promise<Message[]>;
 }
 
--- src/lib/structures/MonitorStore.ts
+++ src/lib/structures/MonitorStore.ts
@@ -1,7 +1,16 @@
 import { Store, Client, PieceConstructor } from '@klasa/core';
-import { Monitor } from '../structures/Monitor';
+import { Monitor } from './Monitor';
 
+/**
+ * Stores all Monitor pieces
+ * @since 0.0.1
+ */
 export class MonitorStore extends Store<Monitor> {
+	/**
+	 * Constructs the Monitor Store for use
+	 * @since 0.0.1
+	 * @param client The framework client
+	 */
 	public constructor(client: Client) {
 		super(client, 'monitors', Monitor as PieceConstructor<Monitor>);
 	}
