07095b89227227fcaf264321b4e7eb03f8553926
QuantumlyTangled <ethoarx@gmail.com>
2020-08-23 10:35:23 +0200

    chore: suggested changes

--- src/lib/types/Events.ts
+++ src/lib/types/Events.ts
@@ -1,29 +1,7 @@
-import type {
-	Channel,
-	Collection,
-	CloseEvent,
-	Guild,
-	GuildEmoji,
-	GuildMember,
-	Invite,
-	Message as DjSMessage,
-	MessageReaction,
-	PartialDMChannel,
-	PartialGuildMember,
-	PartialMessage,
-	PartialUser,
-	Presence,
-	RateLimitData,
-	Role,
-	Snowflake,
-	Speaking,
-	TextChannel,
-	User,
-	VoiceState
-} from 'discord.js';
+import type { Message as DjSMessage } from 'discord.js';
 import type { Command } from '../structures/Command';
 
-export const enum Events {
+export enum Events {
 	// #region Discord.js base events
 	ChannelCreate = 'channelCreate',
 	ChannelDelete = 'channelDelete',
@@ -88,67 +66,16 @@ export const enum Events {
 	// #endregion Sapphire load cycle events
 }
 
-export interface EventParameters {
-	// #region Discord.js base events
-	[Events.ChannelCreate]: [Channel];
-	[Events.ChannelDelete]: [Channel | PartialDMChannel];
-	[Events.ChannelPinsUpdate]: [Channel | PartialDMChannel, Date];
-	[Events.ChannelUpdate]: [Channel, Channel];
-	[Events.Debug]: [string];
-	[Events.Warn]: [string];
-	[Events.Disconnect]: [unknown, number];
-	[Events.EmojiCreate]: [GuildEmoji];
-	[Events.EmojiDelete]: [GuildEmoji];
-	[Events.EmojiUpdate]: [GuildEmoji, GuildEmoji];
-	[Events.Error]: [Error | string];
-	[Events.GuildBanAdd]: [Guild, User | PartialUser];
-	[Events.GuildBanRemove]: [Guild, User | PartialUser];
-	[Events.GuildCreate]: [Guild];
-	[Events.GuildDelete]: [Guild];
-	[Events.GuildUnavailable]: [Guild];
-	[Events.GuildIntegrationsUpdate]: [Guild];
-	[Events.GuildMemberAdd]: [GuildMember | PartialGuildMember];
-	[Events.GuildMemberAvailable]: [GuildMember | PartialGuildMember];
-	[Events.GuildMemberRemove]: [GuildMember | PartialGuildMember];
-	[Events.GuildMembersChunk]: [Collection<Snowflake, GuildMember | PartialGuildMember>, Guild];
-	[Events.GuildMemberSpeaking]: [GuildMember | PartialGuildMember, Readonly<Speaking>];
-	[Events.GuildMemberUpdate]: [GuildMember | PartialGuildMember, GuildMember | PartialGuildMember];
-	[Events.GuildUpdate]: [Guild, Guild];
-	[Events.InviteCreate]: [Invite];
-	[Events.InviteDelete]: [Invite];
-	[Events.Message]: [DjSMessage];
-	[Events.MessageDelete]: [DjSMessage | PartialMessage];
-	[Events.MessageReactionRemoveAll]: [DjSMessage | PartialMessage];
-	[Events.MessageReactionRemoveEmoji]: [MessageReaction];
-	[Events.MessageDeleteBulk]: [Collection<Snowflake, DjSMessage | PartialMessage>];
-	[Events.MessageReactionAdd]: [MessageReaction, User | PartialUser];
-	[Events.MessageReactionRemove]: [MessageReaction, User | PartialUser];
-	[Events.MessageUpdate]: [DjSMessage | PartialMessage, DjSMessage | PartialMessage];
-	[Events.PresenceUpdate]: [Presence | undefined, Presence];
-	[Events.RateLimit]: [RateLimitData];
-	[Events.Ready]: [];
-	[Events.Invalidated]: [];
-	[Events.RoleCreate]: [Role];
-	[Events.RoleDelete]: [Role];
-	[Events.RoleUpdate]: [Role, Role];
-	[Events.TypingsStart]: [Channel | PartialDMChannel, User | PartialUser];
-	[Events.UserUpdate]: [User | PartialUser, User | PartialUser];
-	[Events.VoiceStateUpdate]: [VoiceState, VoiceState];
-	[Events.WebhookUpdate]: [TextChannel];
-	[Events.ShardDisconnect]: [CloseEvent, number];
-	[Events.ShardError]: [Error, number];
-	[Events.SharedReady]: [number];
-	[Events.ShardReconnecting]: [number];
-	[Events.ShardResume]: [number, number];
-	// #endregion Discord.js base events
-
-	// #region Sapphire load cycle events
-	[Events.Unload]: [unknown, unknown];
-	[Events.PostLoad]: [unknown, unknown];
-	[Events.MentionPrefixOnly]: [DjSMessage];
-	[Events.PrefixedMessage]: [DjSMessage, string];
-	[Events.UnknownCommandName]: [DjSMessage, string];
-	[Events.UnknownCommand]: [DjSMessage, string, string];
-	[Events.PreCommandRun]: [DjSMessage, Command, string, string];
-	// #endregion Sapphire load cycle events
+declare module 'discord.js' {
+	interface ClientEvents {
+		// #region Sapphire load cycle events
+		[Events.Unload]: [unknown, unknown];
+		[Events.PostLoad]: [unknown, unknown];
+		[Events.MentionPrefixOnly]: [DjSMessage];
+		[Events.PrefixedMessage]: [DjSMessage, string];
+		[Events.UnknownCommandName]: [DjSMessage, string];
+		[Events.UnknownCommand]: [DjSMessage, string, string];
+		[Events.PreCommandRun]: [DjSMessage, Command, string, string];
+		// #endregion Sapphire load cycle events
+	}
 }
