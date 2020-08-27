5924a53edeb1daea3046c8198e7798dc8932402c
Antonio Román <kyradiscord@gmail.com>
2020-08-27 23:05:52 +0200

    feat(args): add Args#repeat

--- src/lib/utils/Args.ts
+++ src/lib/utils/Args.ts
@@ -3,7 +3,7 @@ import type * as Lexure from 'lexure';
 import { UserError } from '../errors/UserError';
 import type { ArgumentContext } from '../structures/Argument';
 import type { Command } from '../structures/Command';
-import { err, isOk, Ok, Result } from './Result';
+import { err, isErr, isOk, ok, Ok, Result } from './Result';
 
 /**
  * The argument parser to be used in [[Command]].
@@ -130,14 +130,71 @@ export class Args {
 	 * @example
 	 * ```typescript
 	 * // !add 2 Hello World!
-	 * const a = await args.pickResult('integer');
-	 * const b = await args.restResult('string', { minimum: 1 });
+	 * const a = await args.pick('integer');
+	 * const b = await args.rest('string', { minimum: 1 });
 	 * await message.channel.send(`The repeated value is... ${b.repeat(a)}!`);
 	 * // Sends "The repeated value is... Hello World!Hello World!"
 	 * ```
 	 */
 	public async rest<K extends keyof ArgType>(type: K, options?: ArgOptions): Promise<ArgType[K]> {
 		const result = await this.restResult(type, options);
+		if (isOk(result)) return result.value;
+		throw result.error;
+	}
+
+	/**
+	 * Retrieves all the following arguments.
+	 * @param type The type of the argument.
+	 * @example
+	 * ```typescript
+	 * // !add 2 Hello World!
+	 * const result = await args.repeatResult('string', { times: 5 });
+	 * if (!result.success) throw new UserError('CountArgumentError', 'You must write up to 5 words.');
+	 *
+	 * await message.channel.send(`You have written ${result.value.length} word(s): ${result.value.join(' ')}`);
+	 * // Sends "You have written 2 word(s): Hello World!"
+	 * ```
+	 */
+	public async repeatResult<K extends keyof ArgType>(type: K, options: RepeatArgOptions = {}): Promise<Result<ArgType[K][], UserError>> {
+		const argument = this.message.client.arguments.get(type);
+		if (!argument) return err(new UserError('UnavailableArgument', `The argument "${type}" was not found.`));
+
+		if (this.parser.finished) return err(new UserError('MissingArguments', 'There are no more arguments.'));
+
+		const output: ArgType[K][] = [];
+		for (let i = 0, times = options.times ?? Infinity; i < times; i++) {
+			const result = await this.parser.singleParseAsync(async (arg) =>
+				argument.run(arg, {
+					message: this.message,
+					command: this.command,
+					...options
+				})
+			);
+			if (result === null) break;
+			if (isErr(result)) {
+				if (output.length === 0) return result;
+				break;
+			}
+
+			output.push(result.value as ArgType[K]);
+		}
+
+		return ok(output);
+	}
+
+	/**
+	 * Similar to [[Args.repeatResult]] but returns the value on success, throwing otherwise.
+	 * @param type The type of the argument.
+	 * @example
+	 * ```typescript
+	 * // !add 2 Hello World!
+	 * const words = await args.repeat('string', { times: 5 });
+	 * await message.channel.send(`You have written ${words.length} word(s): ${words.join(' ')}`);
+	 * // Sends "You have written 2 word(s): Hello World!"
+	 * ```
+	 */
+	public async repeat<K extends keyof ArgType>(type: K, options?: RepeatArgOptions): Promise<ArgType[K][]> {
+		const result = await this.repeatResult(type, options);
 		if (isOk(result)) return result.value;
 		throw result.error;
 	}
@@ -165,3 +222,11 @@ export interface ArgType {
 }
 
 export interface ArgOptions extends Omit<ArgumentContext, 'message' | 'command'> {}
+
+export interface RepeatArgOptions extends ArgOptions {
+	/**
+	 * The maximum amount of times the argument can be repeated.
+	 * @default Infinity
+	 */
+	times?: number;
+}
