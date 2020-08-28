6744e8f213d8fa8782e75205be1c992c6e1a7f93
LeonardSSH <leonardssh22@gmail.com>
2020-08-28 15:43:02 +0200

    fix(core): arguments docs

--- src/lib/SapphireClient.ts
+++ src/lib/SapphireClient.ts
@@ -32,7 +32,7 @@ export class SapphireClient extends Client {
 	public logger: ILogger;
 
 	/**
-	 * The commands the framework has registered.
+	 * The commands arguments the framework has registered.
 	 * @since 1.0.0
 	 */
 	public arguments: ArgumentStore;
