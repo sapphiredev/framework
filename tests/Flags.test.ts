db43c8fd49a3f76f7e247d482abb94a681694416
Antonio Román <kyradiscord@gmail.com>
2020-09-16 23:24:33 +0200

    test: make messages more accurate

--- tests/Flags.test.ts
+++ tests/Flags.test.ts
@@ -15,36 +15,36 @@ const parse = (testString: string) =>
 		.parse();
 
 describe('Flag parsing strategy', () => {
-	test('GIVEN typeof FlagStrategy RETURNS function', () => {
+	test('GIVEN typeof FlagStrategy THEN returns function', () => {
 		expect(typeof FlagUnorderedStrategy).toBe('function');
 	});
-	test('GIVEN value-less flag RETURNS flag', () => {
+	test('GIVEN flag without value THEN returns flag', () => {
 		const { flags, options } = parse('-f');
 		expect(flags.size).toBe(1);
 		expect([...flags]).toStrictEqual(['f']);
 		expect(options.size).toBe(0);
 	});
 
-	test('GIVEN value-less flag inside text RETURNS flag', () => {
+	test('GIVEN flag without value inside text THEN returns flag', () => {
 		const { flags, options } = parse('commit "hello there" -f');
 		expect(flags.size).toBe(1);
 		expect([...flags]).toStrictEqual(['f']);
 		expect(options.size).toBe(0);
 	});
 
-	test('GIVEN flag with value RETURNS flag', () => {
+	test('GIVEN flag with value THEN returns nothing', () => {
 		const { flags, options } = parse('-f=hi');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(0);
 	});
 
-	test('GIVEN flag with value inside text RETURNS flag', () => {
+	test('GIVEN flag with value inside text THEN returns nothing', () => {
 		const { flags, options } = parse('commit "hello there" -f=hi');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(0);
 	});
 
-	test('GIVEN option with value RETURNS option', () => {
+	test('GIVEN option with value THEN returns option', () => {
 		const { flags, options } = parse('--option=world');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(1);
@@ -52,7 +52,7 @@ describe('Flag parsing strategy', () => {
 		expect(options.get('option')).toStrictEqual(['world']);
 	});
 
-	test('GIVEN option with value inside text RETURNS option', () => {
+	test('GIVEN option with value inside text THEN returns option with single value', () => {
 		const { flags, options } = parse('command --option=world');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(1);
@@ -60,7 +60,7 @@ describe('Flag parsing strategy', () => {
 		expect(options.get('option')).toStrictEqual(['world']);
 	});
 
-	test('GIVEN double-hyphen option with multiple occurences inside text with value RETURNS option with multiple values', () => {
+	test('GIVEN option with multiple occurences inside text THEN returns option with multiple values', () => {
 		const { flags, options } = parse('command --option=world --option=sammy');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(1);
@@ -68,13 +68,13 @@ describe('Flag parsing strategy', () => {
 		expect(options.get('option')).toStrictEqual(['world', 'sammy']);
 	});
 
-	test('GIVEN single-hypen flag inside quotes RETURNS nothing', () => {
+	test('GIVEN flag inside quotes THEN returns nothing', () => {
 		const { flags, options } = parse('commit "hello there -f"');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(0);
 	});
 
-	test('GIVEN double-hyphen option without value inside quote RETURNS nothing', () => {
+	test('GIVEN option without value inside quote THEN returns nothing', () => {
 		const { flags, options } = parse('mention "try --hello"');
 		expect(flags.size).toBe(0);
 		expect(options.size).toBe(0);
