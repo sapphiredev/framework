eabf1a535cd254b1b42ea85d70dcdba6318a9e02
Antonio Román <kyradiscord@gmail.com>
2020-09-17 10:48:55 +0200

    fix: do not get DM channels from guild

--- src/arguments/CoreDMChannel.ts
+++ src/arguments/CoreDMChannel.ts
@@ -1,14 +1,14 @@
 import type { PieceContext } from '@sapphire/pieces';
 import type { DMChannel } from 'discord.js';
-import { Argument, ArgumentContext, ArgumentResult } from '../lib/structures/Argument';
+import { Argument, ArgumentResult } from '../lib/structures/Argument';
 
 export class CoreArgument extends Argument<DMChannel> {
 	public constructor(context: PieceContext) {
 		super(context, { name: 'dmChannel' });
 	}
 
-	public run(argument: string, context: ArgumentContext): ArgumentResult<DMChannel> {
-		const channel = (context.message.guild ? context.message.guild.channels : this.client.channels).cache.get(argument);
+	public run(argument: string): ArgumentResult<DMChannel> {
+		const channel = this.client.channels.cache.get(argument);
 
 		if (!channel) {
 			return this.error(argument, 'ArgumentChannelMissingChannel', 'The argument did not resolve to a channel.');
