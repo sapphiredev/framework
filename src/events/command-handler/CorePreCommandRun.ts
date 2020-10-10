db3e7f4cdb3870e85d9c393a8a5ef5e0faf25b00
Soumil <soumil.paranjpay@gmail.com>
2020-10-10 21:57:18 +0530

    fix: do not run disabled commands

--- src/events/command-handler/CorePreCommandRun.ts
+++ src/events/command-handler/CorePreCommandRun.ts
@@ -1,4 +1,5 @@
 import type { PieceContext } from '@sapphire/pieces';
+import { UserError } from '../../lib/errors/UserError';
 import { Event } from '../../lib/structures/Event';
 import { Events, PreCommandRunPayload } from '../../lib/types/Events';
 import { isErr } from '../../lib/utils/Result';
@@ -9,6 +10,15 @@ export class CoreEvent extends Event<Events.PreCommandRun> {
 	}
 
 	public async run({ message, command, parameters, commandName, prefix }: PreCommandRunPayload) {
+		if (!command.enabled) {
+			this.client.emit(Events.CommandDenied, new UserError('CommandDisabled', 'This command is disabled.'), {
+				message,
+				command,
+				parameters,
+				commandName,
+				prefix
+			});
+		}
 		const result = await command.preconditions.run(message, command);
 		if (isErr(result)) {
 			this.client.emit(Events.CommandDenied, result.error, { message, command, parameters, commandName, prefix });
