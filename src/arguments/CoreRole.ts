9030c49842a9a1c6e1748778f817fee588d5ddc6
Tyler J Russell <22531310+Nytelife26@users.noreply.github.com>
2020-10-24 02:28:33 +0100

    feat(arguments): enable mention and query parsing for role

--- src/arguments/CoreRole.ts
+++ src/arguments/CoreRole.ts
@@ -1,23 +1,61 @@
 import type { PieceContext } from '@sapphire/pieces';
-import type { Role } from 'discord.js';
-import { Argument, ArgumentContext, ArgumentResult } from '../lib/structures/Argument';
+import { Guild, Role } from 'discord.js';
+import { Argument, ArgumentContext, AsyncArgumentResult } from '../lib/structures/Argument';
 
 export class CoreArgument extends Argument<Role> {
 	public constructor(context: PieceContext) {
 		super(context, { name: 'role' });
 	}
-
-	public run(argument: string, context: ArgumentContext): ArgumentResult<Role> {
+	
+	private async parseID(argument: string, guild: Guild): Promise<Role|undefined> {
+		if (/^\d+$/.test(argument)) {
+			return await guild.roles.fetch(argument)
+				.catch({
+					// noop
+				});
+		}
+		return undefined;
+	}
+	
+	private async parseMention(argument: string, guild: Guild): Promise<Role|undefined> {
+		if (/^<@&!*\d+>$/.test(argument)) {
+			return await this.parseID(
+				argument
+					.replace("<@&", "")
+					.replace("!", "")
+					.replace(">", ""),
+				guild
+			);
+		}
+		return undefined;
+	}
+	
+	private async parseQuery(argument: string, guild: Guild): Promise<GuildMember|undefined> {
+		const role = await guild.roles.cache
+			.find((role) => {
+				return role.name.toLowerCase() === argument.toLowerCase()
+			});
+		return role ? role : undefined
+	}
+	
+	public async run(argument: string, context: ArgumentContext): AsyncArgumentResult<GuildMember> {
 		const { guild } = context.message;
 		if (!guild) {
-			return this.error(argument, 'ArgumentRoleMissingGuild', 'The argument must be run on a guild.');
+			return this.error(
+				argument,
+				"ArgumentRoleMissingGuild",
+				"The argument must be run on a guild."
+			);
 		}
 
-		const role = guild.roles.cache.get(argument);
-		if (!role) {
-			return this.error(argument, 'ArgumentRoleMissingRole', 'The argument did not resolve to a role.');
-		}
+		const role = await this.parseID(argument, guild)
+			?? await this.parseMention(argument, guild)
+			?? await this.parseQuery(argument, guild);
 
-		return this.ok(role);
+		return role ? this.ok(role) : this.error(
+			argument,
+			"ArgumentRoleUnknownRole",
+			"The argument did not resolve to a role."
+		);
 	}
 }
