968a1207b08c239bf7de418231035f99c1b04fac
Tyler J Russell <22531310+Nytelife26@users.noreply.github.com>
2020-10-24 02:21:20 +0100

    feat(arguments): enable mention and query parsing for member

--- src/arguments/CoreMember.ts
+++ src/arguments/CoreMember.ts
@@ -1,26 +1,62 @@
 import type { PieceContext } from '@sapphire/pieces';
-import { Constants, DiscordAPIError, GuildMember } from 'discord.js';
+import { GuildMember, Guild } from 'discord.js';
 import { Argument, ArgumentContext, AsyncArgumentResult } from '../lib/structures/Argument';
 
 export class CoreArgument extends Argument<GuildMember> {
 	public constructor(context: PieceContext) {
 		super(context, { name: 'member' });
 	}
-
+	
+	private async parseID(argument: string, guild: Guild): Promise<GuildMember|undefined> {
+		if (/^\d+$/.test(argument)) {
+			try {
+				return await guild.members.fetch(argument);
+			} catch {
+				// noop
+			}
+		}
+		return undefined;
+	}
+	
+	private async parseMention(argument: string, guild: Guild): Promise<GuildMember|undefined> {
+		if (/^<@!*\d+>$/.test(argument)) {
+			return await this.parseID(
+				argument
+					.replace("<@", "")
+					.replace("!", "")
+					.replace(">", ""),
+				guild
+			);
+		}
+		return undefined;
+	}
+	
+	private async parseQuery(argument: string, guild: Guild): Promise<GuildMember|undefined> {
+		const member = await guild.members.fetch({
+			query: argument,
+			limit: 1
+		});
+		return member.values().next().value ?? undefined;
+	}
+	
 	public async run(argument: string, context: ArgumentContext): AsyncArgumentResult<GuildMember> {
 		const { guild } = context.message;
 		if (!guild) {
-			return this.error(argument, 'ArgumentMemberMissingGuild', 'The argument must be run on a guild.');
+			return this.error(
+				argument,
+				'ArgumentMemberMissingGuild',
+				'The argument must be run on a guild.'
+			);
 		}
 
-		try {
-			return this.ok(await guild.members.fetch(argument));
-		} catch (error) {
-			if (error instanceof DiscordAPIError && error.code === Constants.APIErrors.UNKNOWN_MEMBER) {
-				return this.error(argument, 'ArgumentMemberUnknownMember', 'The argument did not resolve to a member.');
-			}
+		const member = await this.parseID(argument, guild)
+			?? await this.parseMention(argument, guild)
+			?? await this.parseQuery(argument, guild);
 
-			return this.error(argument, 'ArgumentMemberUnknownError', 'The argument found an unexpected error when retrieving a member.');
-		}
+		return member ? this.ok(member) : this.error(
+			argument,
+			"ArgumentMemberUnknownMember",
+			"The argument did not resolve to a member."
+		);
 	}
 }
