c4c25c3ac6817ec35004570005b8f120911a68a6
Soumil07 <soumil.paranjpay@gmail.com>
2020-11-15 19:17:20 +0530

    feat(arguments): add CoreBoolean argument (#96)
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

---
+++ src/arguments/CoreBoolean.ts
@@ -0,0 +1,19 @@
+import type { PieceContext } from '@sapphire/pieces';
+import { Argument, ArgumentResult } from '../lib/structures/Argument';
+
+const truths = ['1', 'true', '+', 't', 'yes', 'y'];
+const falses = ['0', 'false', '-', 'f', 'no', 'n'];
+
+export class CoreArgument extends Argument<boolean> {
+	public constructor(context: PieceContext) {
+		super(context, { name: 'boolean' });
+	}
+
+	public run(argument: string): ArgumentResult<boolean> {
+		const boolean = argument.toLowerCase();
+		if (truths.includes(boolean)) return this.ok(true);
+		if (falses.includes(boolean)) return this.ok(false);
+
+		return this.error('ArgumentBooleanInvalidBoolean', 'The argument did not resolve to a boolean.');
+	}
+}
