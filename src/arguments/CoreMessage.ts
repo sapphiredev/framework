d55968359cb91fef30aee5ccf4852462c0a1771d
sadie.lieder <sadie.lieder@gmail.com>
2020-11-23 01:23:13 -0500

    Extend ArgumentContext to include an optional channel property

--- src/arguments/CoreMessage.ts
+++ src/arguments/CoreMessage.ts
@@ -2,6 +2,10 @@ import type { PieceContext } from '@sapphire/pieces';
 import type { DMChannel, Message, NewsChannel, TextChannel } from 'discord.js';
 import { Argument, ArgumentContext, AsyncArgumentResult } from '../lib/structures/Argument';
 
+export interface MessageArgumentContext extends ArgumentContext {
+	channel?: DMChannel | NewsChannel | TextChannel;
+}
+
 export class CoreArgument extends Argument<Message> {
 	private readonly snowflakeRegex = /^\d{17,19}$/;
 
@@ -9,8 +13,8 @@ export class CoreArgument extends Argument<Message> {
 		super(context, { name: 'message' });
 	}
 
-	public async run(argument: string, context: ArgumentContext): AsyncArgumentResult<Message> {
-		const channel = (context.channel as DMChannel | NewsChannel | TextChannel | undefined) ?? context.message.channel;
+	public async run(argument: string, context: MessageArgumentContext): AsyncArgumentResult<Message> {
+		const channel = context.channel ?? context.message.channel;
 		const message = this.snowflakeRegex.test(argument) ? await channel.messages.fetch(argument).catch(() => null) : null;
 		return message ? this.ok(message) : this.error(argument, 'ArgumentMessageUnknownMessage', 'The argument did not resolve to a message.');
 	}
