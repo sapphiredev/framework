5a2e37a91435fcff94ef608d12d083ec8ea7932f
sadie.lieder <sadie.lieder@gmail.com>
2020-12-11 09:34:52 -0500

    Allow specifying direct message links for same-guild viewable channels

--- src/arguments/CoreMessage.ts
+++ src/arguments/CoreMessage.ts
@@ -1,5 +1,5 @@
 import type { PieceContext } from '@sapphire/pieces';
-import type { DMChannel, Message, NewsChannel, TextChannel } from 'discord.js';
+import { DMChannel, Message, NewsChannel, Permissions, TextChannel } from 'discord.js';
 import { Argument, ArgumentContext, AsyncArgumentResult } from '../lib/structures/Argument';
 
 export interface MessageArgumentContext extends ArgumentContext {
@@ -7,6 +7,7 @@ export interface MessageArgumentContext extends ArgumentContext {
 }
 
 export class CoreArgument extends Argument<Message> {
+	private readonly messageLinkRegex = /^(?:https:\/\/)?(?:ptb\.|canary\.)?discord(?:app)?.com\/channels\/(\d{17,19})\/(\d{17,19})\/(\d{17,19})$/;
 	private readonly snowflakeRegex = /^\d{17,19}$/;
 
 	public constructor(context: PieceContext) {
@@ -14,8 +15,33 @@ export class CoreArgument extends Argument<Message> {
 	}
 
 	public async run(argument: string, context: MessageArgumentContext): AsyncArgumentResult<Message> {
-		const channel = context.channel ?? context.message.channel;
-		const message = this.snowflakeRegex.test(argument) ? await channel.messages.fetch(argument).catch(() => null) : null;
+		const message = (await this.resolveByID(argument, context)) ?? (await this.resolveByLink(argument, context));
 		return message ? this.ok(message) : this.error(argument, 'ArgumentMessageUnknownMessage', 'The argument did not resolve to a message.');
+	}
+
+	private async resolveByID(argument: string, context: MessageArgumentContext): Promise<Message | null> {
+		const channel = context.channel ?? context.message.channel;
+		return this.snowflakeRegex.test(argument) ? channel.messages.fetch(argument).catch(() => null) : null;
+	}
+
+	// Placeholder, but credits go to kyranet and developers of skyra-project/skyra:
+	// https://github.com/skyra-project/skyra/blob/main/src/commands/Tools/quote.ts#L51
+	private async resolveByLink(argument: string, { message }: MessageArgumentContext): Promise<Message | null> {
+		if (!message.guild) return null;
+
+		const matches = this.messageLinkRegex.exec(argument);
+		if (!matches) return null;
+		const [, guildID, channelID, messageID] = matches;
+
+		const guild = this.client.guilds.cache.get(guildID);
+		if (guild !== message.guild) return null;
+
+		const channel = guild.channels.cache.get(channelID);
+		if (!channel) return null;
+		if (!(channel instanceof NewsChannel || channel instanceof TextChannel)) return null;
+		if (!channel.viewable) return null;
+		if (!channel.permissionsFor(message.author)?.has(Permissions.FLAGS.VIEW_CHANNEL)) return null;
+
+		return channel.messages.fetch(messageID).catch(() => null);
 	}
 }
