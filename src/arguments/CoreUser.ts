1cc82f4a41583220b373da1ed7b649f54ffeb8b2
sadie.lieder <sadie.lieder@gmail.com>
2020-12-22 21:06:33 -0500

    feat(arguments): Add Message argument, use discord-utilities

--- src/arguments/CoreUser.ts
+++ src/arguments/CoreUser.ts
@@ -1,16 +1,15 @@
+import { SnowflakeRegex, UserOrMemberMentionRegex } from '@sapphire/discord-utilities';
 import type { PieceContext } from '@sapphire/pieces';
 import type { User } from 'discord.js';
 import { Argument, AsyncArgumentResult } from '../lib/structures/Argument';
 
 export class CoreArgument extends Argument<User> {
-	private readonly userOrMemberRegex = /^(?:<@!?)?(\d{17,19})>?$/;
-
 	public constructor(context: PieceContext) {
 		super(context, { name: 'user' });
 	}
 
 	public async run(argument: string): AsyncArgumentResult<User> {
-		const userID = this.userOrMemberRegex.exec(argument);
+		const userID = UserOrMemberMentionRegex.exec(argument) ?? SnowflakeRegex.exec(argument);
 		const user = userID ? await this.context.client.users.fetch(userID[1]).catch(() => null) : null;
 		return user ? this.ok(user) : this.error(argument, 'ArgumentUserUnknownUser', 'The argument did not resolve to a user.');
 	}
