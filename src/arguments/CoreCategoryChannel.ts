4c281a33c2852bee3daac160ed76f0cc2ab9b0ad
Soumil07 <soumil.paranjpay@gmail.com>
2021-01-20 16:04:04 +0530

    feat(arguments): add CoreCategoryChannel (#136)

---
+++ src/arguments/CoreCategoryChannel.ts
@@ -0,0 +1,20 @@
+import { isCategoryChannel } from '@sapphire/discord.js-utilities';
+import type { PieceContext } from '@sapphire/pieces';
+import type { CategoryChannel, GuildChannel } from 'discord.js';
+import type { ArgumentResult } from '../lib/structures/Argument';
+import { ExtendedArgument, ExtendedArgumentContext } from '../lib/structures/ExtendedArgument';
+
+export class CoreArgument extends ExtendedArgument<'guildChannel', CategoryChannel> {
+	public constructor(context: PieceContext) {
+		super(context, {
+			name: 'categoryChannel',
+			baseArgument: 'guildChannel'
+		});
+	}
+
+	public handle(channel: GuildChannel, { argument }: ExtendedArgumentContext): ArgumentResult<CategoryChannel> {
+		return isCategoryChannel(channel)
+			? this.ok(channel)
+			: this.error(argument, 'ArgumentCategoryChannelInvalidChannel', 'The argument did not resolve to a category channel.');
+	}
+}
