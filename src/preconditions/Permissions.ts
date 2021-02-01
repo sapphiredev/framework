f3e3eefa7b6d448e4fa6b83f75f0b88bbe980170
Soumil <soumildiscord@gmail.com>
2021-02-01 21:10:57 +0530

    refactor: make readablePermissions a class field

--- src/preconditions/Permissions.ts
+++ src/preconditions/Permissions.ts
@@ -25,43 +25,43 @@ export class CorePrecondition extends Precondition {
 			? this.ok()
 			: this.error({
 					message: `I am missing the following permissions to run this command: ${missing
-						.map((perm) => readablePermissions[perm])
+						.map((perm) => CorePrecondition.readablePermissions[perm])
 						.join(', ')}`,
 					context: { missing }
 			  });
 	}
-}
 
-const readablePermissions = {
-	ADMINISTRATOR: 'Administrator',
-	VIEW_AUDIT_LOG: 'View Audit Log',
-	MANAGE_GUILD: 'Manage Server',
-	MANAGE_ROLES: 'Manage Roles',
-	MANAGE_CHANNELS: 'Manage Channels',
-	KICK_MEMBERS: 'Kick Members',
-	BAN_MEMBERS: 'Ban Members',
-	CREATE_INSTANT_INVITE: 'Create Instant Invite',
-	CHANGE_NICKNAME: 'Change Nickname',
-	MANAGE_NICKNAMES: 'Manage Nicknames',
-	MANAGE_EMOJIS: 'Manage Emojis',
-	MANAGE_WEBHOOKS: 'Manage Webhooks',
-	VIEW_CHANNEL: 'Read Messages',
-	SEND_MESSAGES: 'Send Messages',
-	SEND_TTS_MESSAGES: 'Send TTS Messages',
-	MANAGE_MESSAGES: 'Manage Messages',
-	EMBED_LINKS: 'Embed Links',
-	ATTACH_FILES: 'Attach Files',
-	READ_MESSAGE_HISTORY: 'Read Message History',
-	MENTION_EVERYONE: 'Mention Everyone',
-	USE_EXTERNAL_EMOJIS: 'Use External Emojis',
-	ADD_REACTIONS: 'Add Reactions',
-	CONNECT: 'Connect',
-	SPEAK: 'Speak',
-	STREAM: 'Stream',
-	MUTE_MEMBERS: 'Mute Members',
-	DEAFEN_MEMBERS: 'Deafen Members',
-	MOVE_MEMBERS: 'Move Members',
-	USE_VAD: 'Use Voice Activity',
-	PRIORITY_SPEAKER: 'Priority Speaker',
-	VIEW_GUILD_INSIGHTS: 'View Guild Insights'
-};
+	protected static readonly readablePermissions = {
+		ADMINISTRATOR: 'Administrator',
+		VIEW_AUDIT_LOG: 'View Audit Log',
+		MANAGE_GUILD: 'Manage Server',
+		MANAGE_ROLES: 'Manage Roles',
+		MANAGE_CHANNELS: 'Manage Channels',
+		KICK_MEMBERS: 'Kick Members',
+		BAN_MEMBERS: 'Ban Members',
+		CREATE_INSTANT_INVITE: 'Create Instant Invite',
+		CHANGE_NICKNAME: 'Change Nickname',
+		MANAGE_NICKNAMES: 'Manage Nicknames',
+		MANAGE_EMOJIS: 'Manage Emojis',
+		MANAGE_WEBHOOKS: 'Manage Webhooks',
+		VIEW_CHANNEL: 'Read Messages',
+		SEND_MESSAGES: 'Send Messages',
+		SEND_TTS_MESSAGES: 'Send TTS Messages',
+		MANAGE_MESSAGES: 'Manage Messages',
+		EMBED_LINKS: 'Embed Links',
+		ATTACH_FILES: 'Attach Files',
+		READ_MESSAGE_HISTORY: 'Read Message History',
+		MENTION_EVERYONE: 'Mention Everyone',
+		USE_EXTERNAL_EMOJIS: 'Use External Emojis',
+		ADD_REACTIONS: 'Add Reactions',
+		CONNECT: 'Connect',
+		SPEAK: 'Speak',
+		STREAM: 'Stream',
+		MUTE_MEMBERS: 'Mute Members',
+		DEAFEN_MEMBERS: 'Deafen Members',
+		MOVE_MEMBERS: 'Move Members',
+		USE_VAD: 'Use Voice Activity',
+		PRIORITY_SPEAKER: 'Priority Speaker',
+		VIEW_GUILD_INSIGHTS: 'View Guild Insights'
+	};
+}
