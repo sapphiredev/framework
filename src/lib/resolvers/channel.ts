54660846bb5b32fa64ece7501c3b7c328335a571
Elliot Maisl <elliot.maisl@gmail.com>
2021-08-24 14:27:16 +0200

    feat(resolver): make resolveChannel parse mentions

--- src/lib/resolvers/channel.ts
+++ src/lib/resolvers/channel.ts
@@ -1,11 +1,12 @@
-import type { ChannelTypes } from '@sapphire/discord.js-utilities';
+import { ChannelMentionRegex, ChannelTypes } from '@sapphire/discord.js-utilities';
 import { container } from '@sapphire/pieces';
-import type { Message, Snowflake } from 'discord.js';
+import type { Message } from 'discord.js';
 import { Identifiers } from '../errors/Identifiers';
 import { err, ok, Result } from '../parsers/Result';
 
 export function resolveChannel(parameter: string, message: Message): Result<ChannelTypes, Identifiers.ArgumentChannelError> {
-	const channel = (message.guild ? message.guild.channels : container.client.channels).cache.get(parameter as Snowflake);
+	const channelId = ChannelMentionRegex.exec(parameter);
+	const channel = (message.guild ? message.guild.channels : container.client.channels).cache.get(channelId?.[1] ?? parameter);
 	if (channel) return ok(channel as ChannelTypes);
 	return err(Identifiers.ArgumentChannelError);
 }
