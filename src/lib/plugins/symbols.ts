d36b10f1face010f251963610e1bcf823a27ad08
Nejc Drobnič <ethoarx@gmail.com>
2021-08-31 10:10:06 +0200

    refactor: use symbol for

--- src/lib/plugins/symbols.ts
+++ src/lib/plugins/symbols.ts
@@ -1,6 +1,6 @@
-export const preGenericsInitialization: unique symbol = Symbol('SapphireFrameworkPluginsPreGenericsInitialization');
-export const preInitialization: unique symbol = Symbol('SapphireFrameworkPluginsPreInitialization');
-export const postInitialization: unique symbol = Symbol('SapphireFrameworkPluginsPostInitialization');
+export const preGenericsInitialization: unique symbol = Symbol.for('SapphireFrameworkPluginsPreGenericsInitialization');
+export const preInitialization: unique symbol = Symbol.for('SapphireFrameworkPluginsPreInitialization');
+export const postInitialization: unique symbol = Symbol.for('SapphireFrameworkPluginsPostInitialization');
 
-export const preLogin: unique symbol = Symbol('SapphireFrameworkPluginsPreLogin');
-export const postLogin: unique symbol = Symbol('SapphireFrameworkPluginsPostLogin');
+export const preLogin: unique symbol = Symbol.for('SapphireFrameworkPluginsPreLogin');
+export const postLogin: unique symbol = Symbol.for('SapphireFrameworkPluginsPostLogin');
