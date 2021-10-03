c32d7cc5a64b379f356baa563022ee92695c3bd8
Elliot Maisl <elliot.maisl@gmail.com>
2021-10-03 19:37:18 +0200

    feat(resolver): make resolveMessage parse channelId-messageId
    When you hold Shift and hover over a message, then click the "ID"
    button, you don't actually get the message id, but rather
    "channelId-messageId".
    This commit makes resolveMessage parse this kind of pattern.

--- src/lib/resolvers/message.ts
+++ src/lib/resolvers/message.ts
@@ -1,8 +1,8 @@
-import { MessageLinkRegex, SnowflakeRegex } from '@sapphire/discord-utilities';
+import { ChannelMessageRegex, MessageLinkRegex, SnowflakeRegex } from '@sapphire/discord-utilities';
 import { GuildBasedChannelTypes, isNewsChannel, isTextChannel, TextBasedChannelTypes } from '@sapphire/discord.js-utilities';
 import { container } from '@sapphire/pieces';
 import type { Awaitable } from '@sapphire/utilities';
-import { Message, Permissions, Snowflake } from 'discord.js';
+import { Message, Permissions, Snowflake, User } from 'discord.js';
 import { Identifiers } from '../errors/Identifiers';
 import { err, ok, Result } from '../parsers/Result';
 
@@ -13,7 +13,10 @@ interface MessageResolverOptions {
 
 export async function resolveMessage(parameter: string, options: MessageResolverOptions): Promise<Result<Message, Identifiers.ArgumentMessageError>> {
 	const channel = options.channel ?? options.message.channel;
-	const message = (await resolveById(parameter, channel)) ?? (await resolveByLink(parameter, options.message));
+	const message =
+		(await resolveById(parameter, channel)) ??
+		(await resolveByLink(parameter, options.message)) ??
+		(await resolveByChannelAndMessage(parameter, options.message));
 	if (message) return ok(message);
 	return err(Identifiers.ArgumentMessageError);
 }
@@ -32,11 +35,22 @@ async function resolveByLink(parameter: string, message: Message): Promise<Messa
 	const guild = container.client.guilds.cache.get(guildId as Snowflake);
 	if (guild !== message.guild) return null;
 
-	const channel = guild.channels.cache.get(channelId as Snowflake) as GuildBasedChannelTypes;
+	return getMessageFromChannel(channelId, messageId, message.author);
+}
+
+async function resolveByChannelAndMessage(parameter: string, message: Message): Promise<Message | null> {
+	const result = ChannelMessageRegex.exec(parameter)?.groups;
+	if (!result) return null;
+
+	return getMessageFromChannel(result.channelId, result.messageId, message.author);
+}
+
+async function getMessageFromChannel(channelId: Snowflake, messageId: Snowflake, originalAuthor: User): Promise<Message | null> {
+	const channel = container.client.channels.cache.get(channelId as Snowflake) as GuildBasedChannelTypes;
 	if (!channel) return null;
 	if (!(isNewsChannel(channel) || isTextChannel(channel))) return null;
 	if (!channel.viewable) return null;
-	if (!channel.permissionsFor(message.author)?.has(Permissions.FLAGS.VIEW_CHANNEL)) return null;
+	if (!channel.permissionsFor(originalAuthor)?.has(Permissions.FLAGS.VIEW_CHANNEL)) return null;
 
-	return channel.messages.fetch(messageId as Snowflake);
+	return channel.messages.fetch(messageId);
 }
