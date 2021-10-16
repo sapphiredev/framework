de34f7b3dfd5216a36e4d8728e0fb3dbed47fc6e
Jeroen Claassens <support@favware.tech>
2021-10-16 16:29:43 +0200

    chore: too tired to write descriptive message
    permalink: http://whatthecommit.com/f3d59e7543bde9231f463f9655d17be5

--- src/lib/resolvers/member.ts
+++ src/lib/resolvers/member.ts
@@ -3,8 +3,6 @@ import type { Guild, GuildMember, Snowflake } from 'discord.js';
 import { Identifiers } from '../errors/Identifiers';
 import { err, ok, Result } from '../parsers/Result';
 
-const memberWithDiscriminatorRegex = /#\d{4}$/;
-
 export async function resolveMember(parameter: string, guild: Guild): Promise<Result<GuildMember, Identifiers.ArgumentMemberError>> {
 	const member = (await resolveById(parameter, guild)) ?? (await resolveByQuery(parameter, guild));
 	if (member) return ok(member);
@@ -17,10 +15,7 @@ async function resolveById(argument: string, guild: Guild): Promise<GuildMember 
 }
 
 async function resolveByQuery(argument: string, guild: Guild): Promise<GuildMember | null> {
-	const queryDiscriminator = memberWithDiscriminatorRegex.test(argument);
-	if (queryDiscriminator) {
-		argument = argument.substring(0, -4);
-	}
+	argument = argument.length > 5 && argument.at(-5) === '#' ? argument.slice(0, -5) : argument;
 
 	const members = await guild.members.fetch({ query: argument, limit: 1 }).catch(() => null);
 	return members?.first() ?? null;
