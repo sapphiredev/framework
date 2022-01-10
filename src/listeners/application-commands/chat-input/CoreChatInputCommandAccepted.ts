03ce93842273350c339d019cbbff76d5ac176e48
Feralheart <feralheart@feralheart.dev>
2022-01-10 21:10:58 +0100

    feat(commands): Add duration to Error and Finnished events

--- src/listeners/application-commands/chat-input/CoreChatInputCommandAccepted.ts
+++ src/listeners/application-commands/chat-input/CoreChatInputCommandAccepted.ts
@@ -1,4 +1,5 @@
 import type { PieceContext } from '@sapphire/pieces';
+import { Stopwatch } from '@sapphire/stopwatch';
 import { fromAsync, isErr } from '../../../lib/parsers/Result';
 import { Listener } from '../../../lib/structures/Listener';
 import { ChatInputCommandAcceptedPayload, Events } from '../../../lib/types/Events';
@@ -11,16 +12,18 @@ export class CoreListener extends Listener<typeof Events.ChatInputCommandAccepte
 	public async run(payload: ChatInputCommandAcceptedPayload) {
 		const { command, context, interaction } = payload;
 
+		const stopwatch = new Stopwatch();
 		const result = await fromAsync(async () => {
 			this.container.client.emit(Events.ChatInputCommandRun, interaction, command, { ...payload });
 			const result = await command.chatInputRun(interaction, context);
 			this.container.client.emit(Events.ChatInputCommandSuccess, { ...payload, result });
 		});
 
+		const { duration } = stopwatch.stop();
 		if (isErr(result)) {
-			this.container.client.emit(Events.ChatInputCommandError, result.error, { ...payload });
+			this.container.client.emit(Events.ChatInputCommandError, result.error, { ...payload, duration });
 		}
 
-		this.container.client.emit(Events.ChatInputCommandFinish, interaction, command, { ...payload });
+		this.container.client.emit(Events.ChatInputCommandFinish, interaction, command, { ...payload, duration });
 	}
 }
