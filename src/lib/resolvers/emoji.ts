924558b0389e796b563555fb3874fa2d19740fb9
Jeroen Claassens <support@favware.tech>
2022-01-10 23:32:57 +0100

    chore: it's 2016; why are we using coldfusion?!
    permalink: http://whatthecommit.com/775efe5c58912e313a520df6d74b133a

--- src/lib/resolvers/emoji.ts
+++ src/lib/resolvers/emoji.ts
@@ -1,19 +1,22 @@
 import { EmojiRegex, TwemojiRegex } from '@sapphire/discord-utilities';
 import { Util } from 'discord.js';
 import { Identifiers } from '../errors/Identifiers';
-import { err, ok } from '../parsers/Result';
+import { Err, err, Ok, ok } from '../parsers/Result';
 
-export function resolveEmoji(parameter: string) {
+export function resolveEmoji(parameter: string): Ok<EmojiObject> | Err<Identifiers> {
 	const twemoji = TwemojiRegex.exec(parameter)?.[0] ?? null;
 
 	if (twemoji) {
-		return ok(twemoji);
+		return ok({
+			name: twemoji,
+			id: null
+		});
 	}
 
-	const emojiId = EmojiRegex.exec(parameter)?.groups?.id;
+	const emojiId = EmojiRegex.test(parameter);
 
 	if (emojiId) {
-		const resolved = Util.parseEmoji(emojiId) as EmojiObject;
+		const resolved = Util.parseEmoji(parameter) as EmojiObject | null;
 
 		if (resolved) {
 			return ok(resolved);
