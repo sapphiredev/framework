35d02d8c05166f766b24bf923a6c1c6698bb5019
Jeroen Claassens <support@favware.tech>
2022-01-14 21:12:36 +0100

    fix(CoreEmoji): export named class instead of default

--- src/arguments/CoreEmoji.ts
+++ src/arguments/CoreEmoji.ts
@@ -3,7 +3,7 @@ import { Identifiers } from '../lib/errors/Identifiers';
 import { EmojiObject, resolveEmoji } from '../lib/resolvers/emoji';
 import { Argument, ArgumentResult } from '../lib/structures/Argument';
 
-export default class extends Argument<EmojiObject> {
+export class CoreArgument extends Argument<EmojiObject> {
 	public constructor(context: PieceContext) {
 		super(context, { name: 'emoji' });
 	}
