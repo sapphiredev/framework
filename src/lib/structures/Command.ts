0befa0388adc931d2babcd9b92a4d8143bacb04d
Lioness100 <jchickmm2@gmail.com>
2022-02-11 11:20:37 -0500

    feat(command): add Interaction and Registry namespace members

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -489,6 +489,8 @@ export namespace ChatInputCommand {
 	export type Context = AliasPiece.Context;
 	export type RunInTypes = CommandOptionsRunType;
 	export type RunContext = ChatInputCommandContext;
+	export type Interaction = CommandInteraction;
+	export type Registry = ApplicationCommandRegistry;
 }
 
 export type ContextMenuCommand = Command & Required<Pick<Command, 'contextMenuRun'>>;
@@ -499,6 +501,8 @@ export namespace ContextMenuCommand {
 	export type Context = AliasPiece.Context;
 	export type RunInTypes = CommandOptionsRunType;
 	export type RunContext = ContextMenuCommandContext;
+	export type Interaction = ContextMenuInteraction;
+	export type Registry = ApplicationCommandRegistry;
 }
 
 export type AutocompleteCommand = Command & Required<Pick<Command, 'autocompleteRun'>>;
@@ -509,6 +513,8 @@ export namespace AutocompleteCommand {
 	export type Context = AliasPiece.Context;
 	export type RunInTypes = CommandOptionsRunType;
 	export type RunContext = AutocompleteCommandContext;
+	export type Interaction = AutocompleteInteraction;
+	export type Registry = ApplicationCommandRegistry;
 }
 
 /**
