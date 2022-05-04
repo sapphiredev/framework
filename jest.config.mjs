210481a735cd186d42fadbe2cb2c9eef71c905a4
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-04 23:45:37 +0530

    chore: bump jest and use mjs (#422)
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

---
+++ jest.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+	displayName: 'unit test',
+	preset: 'ts-jest',
+	testMatch: ['<rootDir>/tests/**/*.test.ts'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tests/tsconfig.json'
+		}
+	},
+	reporters: ['default', 'github-actions']
+};
+
+export default config;
