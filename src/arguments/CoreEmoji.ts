b9a88638cd29b33792f50eed8039dbe2e56e0cc5
samfundev <samdinner@hotmail.com>
2022-08-03 18:51:25 -0400

    fix: add context to emoji arguments

--- src/arguments/CoreEmoji.ts
+++ src/arguments/CoreEmoji.ts
@@ -7,13 +7,14 @@ export class CoreArgument extends Argument<EmojiObject> {
 		super(context, { name: 'emoji' });
 	}
 
-	public run(parameter: string): ArgumentResult<EmojiObject> {
+	public run(parameter: string, context: Argument.Context): ArgumentResult<EmojiObject> {
 		const resolved = resolveEmoji(parameter);
 		return resolved.mapErrInto((identifier) =>
 			this.error({
 				parameter,
 				identifier,
-				message: 'The argument did not resolve to an emoji.'
+				message: 'The argument did not resolve to an emoji.',
+				context
 			})
 		);
 	}
