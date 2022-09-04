9958229b8b6a171de66d0fe9afa67ca0164c5280
Vlad Frangu <kingdgrizzle@gmail.com>
2022-09-04 17:31:28 +0300

    chore: use reporters from shapeshift

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		globals: true
+		globals: true,
+		coverage: {
+			reporter: ['text', 'lcov', 'clover']
+		}
 	},
 	esbuild: {
 		target: 'es2022'
