de787ffa135eea2a08d50ed73b5a9712598fdacb
Vlad Frangu <kingdgrizzle@gmail.com>
2022-09-04 17:24:09 +0300

    chore: axe jest, bring in vitest

--- tests/Command.test.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from '../src';
-
-describe('Command tests', () => {
-	test('GIVEN typeof Command THEN returns function', () => {
-		expect(typeof Command).toBe('function');
-	});
-});
