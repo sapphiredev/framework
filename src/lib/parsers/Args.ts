222d50e0459f7769bea660ae9ae5ce412b927cdb
yuansheng1549 <yuansheng1549@gmail.com>
2023-04-16 21:45:34 +0800

    fix(ArgType): update `type` for guild*ThreadChannel (#628)
    * fix(ArgType): remove `type` for guild*ThreadChannel
    
    * chore(ArgsType): use `ChannelType` instead of removing the `type`

--- src/lib/parsers/Args.ts
+++ src/lib/parsers/Args.ts
@@ -5,6 +5,7 @@ import { Option, Result } from '@sapphire/result';
 import type { Awaitable } from '@sapphire/utilities';
 import type {
 	CategoryChannel,
+	ChannelType,
 	DMChannel,
 	GuildMember,
 	Message,
@@ -782,9 +783,9 @@ export interface ArgType {
 	guildCategoryChannel: CategoryChannel;
 	guildChannel: GuildBasedChannelTypes;
 	guildNewsChannel: NewsChannel;
-	guildNewsThreadChannel: ThreadChannel & { type: 'GUILD_NEWS_THREAD'; parent: NewsChannel | null };
-	guildPrivateThreadChannel: ThreadChannel & { type: 'GUILD_PRIVATE_THREAD'; parent: TextChannel | null };
-	guildPublicThreadChannel: ThreadChannel & { type: 'GUILD_PUBLIC_THREAD'; parent: TextChannel | null };
+	guildNewsThreadChannel: ThreadChannel & { type: ChannelType.AnnouncementThread; parent: NewsChannel | null };
+	guildPrivateThreadChannel: ThreadChannel & { type: ChannelType.PrivateThread; parent: TextChannel | null };
+	guildPublicThreadChannel: ThreadChannel & { type: ChannelType.PublicThread; parent: TextChannel | null };
 	guildStageVoiceChannel: StageChannel;
 	guildTextChannel: TextChannel;
 	guildThreadChannel: ThreadChannel;
