d3c6b0b833058a91147b1735071ddfcfd6d3074f
Jeroen Claassens <jeroen.claassens@rws.nl>
2023-10-23 11:20:01 +0200

    chore: revert this commit

--- src/lib/precondition-resolvers/userPermissions.ts
+++ src/lib/precondition-resolvers/userPermissions.ts
@@ -5,7 +5,7 @@ import type { PreconditionContainerArray } from '../utils/preconditions/Precondi
 /**
  * Appends the `UserPermissions` precondition when {@link Command.Options.requiredUserPermissions} resolves to a
  * non-zero bitfield.
- * @param requiredClientPermissions The required user permissions.
+ * @param requiredUserPermissions The required user permissions.
  * @param preconditionContainerArray The precondition container array to append the precondition to.
  */
 export function parseConstructorPreConditionsRequiredUserPermissions(
