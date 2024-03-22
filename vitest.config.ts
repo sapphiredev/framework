069aee6a8cec0fd7b357866e1faec4234e0470ef
Jeroen Claassens <support@favware.tech>
2024-03-23 00:47:29 +0100

    ci: remove custom reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		reporters: ['default', ['vitest-sonar-reporter', { outputFile: 'sonar-report.xml' }]],
 		coverage: {
 			enabled: true,
 			reporter: ['text', 'lcov', 'cobertura']
