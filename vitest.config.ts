95760d72bca77e21051a4fea36d8a2753f51d798
Jeroen Claassens <support@favware.tech>
2024-03-23 00:36:38 +0100

    ci: have sonar read test coverage and test report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
+		reporters: ['default', ['vitest-sonar-reporter', { outputFile: 'sonar-report.xml' }]],
 		coverage: {
 			enabled: true,
 			reporter: ['text', 'lcov', 'cobertura']
