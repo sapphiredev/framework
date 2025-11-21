03cfd66a12ae362be4384596c2d79fca2455a392
Jeroen Claassens <jeroen.claassens@rws.nl>
2025-11-21 10:17:54 +0100

    build: swap from tsup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, type UserConfig } from 'tsdown';
+import Replace from 'unplugin-replace/rolldown';
+import { version } from './package.json';
+
+const baseOptions: UserConfig = {
+	clean: true,
+	entry: ['src/**/*.ts'],
+	dts: true,
+	unbundle: true,
+	minify: false,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'es2021',
+	tsconfig: 'src/tsconfig.json',
+	plugins: [
+		Replace({
+			values: [
+				{
+					find: /\[VI\]{{inject}}\[\/VI\]/,
+					replacement: version
+				}
+			]
+		})
+	],
+	treeshake: true
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs'
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm'
+	})
+];
